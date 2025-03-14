4e5224d0eb373a38b61b0e08e662b4dcedf06e33
titophil <kipronotitus254@gmail.com>
2025-03-14 15:15:24 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let input = document.getElementById('button')
 function addingEventListener() {
-}
+    if(input){
+        input.addEventListener('click',function(){
+        alert('I was clicked!')
+        })
+    }
+        else{
+            console.error('No event!')
+
+        }
+
+    }
+
+
+
+
